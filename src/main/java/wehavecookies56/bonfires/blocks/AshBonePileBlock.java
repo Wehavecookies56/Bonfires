4SV-Studio/Bonfires-HOTU

GUI syncing, small fixes, Screenshots, Timestamps, Tab tooltips Bonfire GUI will update in real time if another player destroys or creates a bonfire. Bonfire GUI will close if the player moves away from the Bonfire or is destroyed. Dimension tabs now have a tooltip. Bonfires store a timestamp of when they were created and sorts the list with the timestamps Bonfire screenshot system added, screenshot will be taken when the bonfire is created or when the button in the GUI is pressed. Screenshots are stored locally and not sent to other players. Added config options for Screenshot system. Added checks to stop duplicate Bonfires from displaying and saving if something goes wrong and a duplicate is created

src/main/java/wehavecookies56/bonfires/blocks/AshBonePileBlock.java
@@ -21,11 +21,7 @@ import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Explosion;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelReader;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.RenderShape;
-import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
@@ -42,17 +38,13 @@ import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 import net.minecraftforge.server.ServerLifecycleHooks;
 import wehavecookies56.bonfires.BonfiresConfig;
+import wehavecookies56.bonfires.bonfire.Bonfire;
 import wehavecookies56.bonfires.bonfire.BonfireRegistry;
 import wehavecookies56.bonfires.data.BonfireHandler;
 import wehavecookies56.bonfires.data.EstusHandler;
 import wehavecookies56.bonfires.data.ReinforceHandler;
 import wehavecookies56.bonfires.packets.PacketHandler;
-import wehavecookies56.bonfires.packets.client.OpenBonfireGUI;
-import wehavecookies56.bonfires.packets.client.OpenCreateScreen;
-import wehavecookies56.bonfires.packets.client.SyncBonfire;
-import wehavecookies56.bonfires.packets.client.SyncEstusData;
-import wehavecookies56.bonfires.packets.client.SyncReinforceData;
-import wehavecookies56.bonfires.packets.client.SyncSaveData;
+import wehavecookies56.bonfires.packets.client.*;
 import wehavecookies56.bonfires.setup.EntitySetup;
 import wehavecookies56.bonfires.setup.ItemSetup;
 import wehavecookies56.bonfires.tiles.BonfireTileEntity;
@@ -144,6 +136,11 @@ public class AshBonePileBlock extends Block implements EntityBlock {
                             PacketHandler.sendTo(new SyncEstusData(EstusHandler.getHandler(player)), (ServerPlayer) player);
                             world.sendBlockUpdated(pos, state, state, Block.UPDATE_CLIENTS);
                             return InteractionResult.SUCCESS;
+                        } else {
+                            //Bonfire lit but not in data, so should not be lit
+                            te.setLit(false);
+                            world.sendBlockUpdated(pos, state, state, Block.UPDATE_CLIENTS);
+                            return InteractionResult.SUCCESS;
                         }
                     } else {
                         return InteractionResult.SUCCESS;
@@ -203,10 +200,13 @@ public class AshBonePileBlock extends Block implements EntityBlock {
                         world.addFreshEntity(fragment);
                     }
                     if (te.isLit()) {
-                        te.destroyBonfire(te.getID());
                         MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+                        Bonfire destroyed = BonfireHandler.getServerHandler(server).getRegistry().getBonfire(te.getID());
+                        te.destroyBonfire(te.getID());
                         BonfireHandler.getServerHandler(server).removeBonfire(te.getID());
                         PacketHandler.sendToAll(new SyncSaveData(BonfireHandler.getServerHandler(server).getRegistry().getBonfires()));
+                        PacketHandler.sendToAll(new SendBonfiresToClient());
+                        PacketHandler.sendToAll(new DeleteScreenshot(te.getID(), destroyed.getName()));
                     }
                 }
             }

src/main/java/wehavecookies56/bonfires/bonfire/Bonfire.java
@@ -4,6 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
 
+import java.time.Instant;
 import java.util.UUID;
 
 /**
@@ -18,13 +19,16 @@ public class Bonfire {
     ResourceKey<Level> dimension;
     boolean isPublic;
 
-    public Bonfire(String name, UUID id, UUID owner, BlockPos pos, ResourceKey<Level> dimension, boolean isPublic) {
+    Instant timeCreated;
+
+    public Bonfire(String name, UUID id, UUID owner, BlockPos pos, ResourceKey<Level> dimension, boolean isPublic, Instant timeCreated) {
         this.name = name;
         this.id = id;
         this.owner = owner;
         this.pos = pos;
         this.dimension = dimension;
         this.isPublic = isPublic;
+        this.timeCreated = timeCreated;
     }
 
     public String getName() {
@@ -73,5 +77,13 @@ public class Bonfire {
 
     public void setPublic(boolean aPublic) {
         isPublic = aPublic;
+    }
+
+    public Instant getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(Instant timeCreated) {
+        this.timeCreated = timeCreated;
     }
 }

src/main/java/wehavecookies56/bonfires/packets/client/SendBonfiresToClient.java
@@ -1,6 +1,7 @@
 package wehavecookies56.bonfires.packets.client;
 
 import net.minecraft.core.registries.Registries;
+import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.level.Level;
@@ -8,26 +9,32 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.network.NetworkEvent;
 import net.minecraftforge.server.ServerLifecycleHooks;
+import wehavecookies56.bonfires.bonfire.BonfireRegistry;
 import wehavecookies56.bonfires.client.ClientPacketHandler;
+import wehavecookies56.bonfires.data.BonfireHandler;
 import wehavecookies56.bonfires.packets.Packet;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SendDimensionsToClient extends Packet<SendDimensionsToClient> {
+public class SendBonfiresToClient extends Packet<SendBonfiresToClient> {
 
-    public SendDimensionsToClient(FriendlyByteBuf buffer) {
+    public SendBonfiresToClient(FriendlyByteBuf buffer) {
         super(buffer);
     }
 
     public List<ResourceKey<Level>> dimensions;
+    public BonfireRegistry registry;
 
-    public SendDimensionsToClient() {
+    public SendBonfiresToClient() {
         dimensions = new ArrayList<>(ServerLifecycleHooks.getCurrentServer().levelKeys());
+        registry = BonfireHandler.getServerHandler(ServerLifecycleHooks.getCurrentServer()).getRegistry();
     }
 
     @Override
     public void decode(FriendlyByteBuf buffer) {
+        registry = new BonfireRegistry();
+        registry.readFromNBT(buffer.readNbt(), registry.getBonfires());
         dimensions = new ArrayList<>();
         int size = buffer.readVarInt();
         for (int i = 0; i < size; i++) {
@@ -37,6 +44,7 @@ public class SendDimensionsToClient extends Packet<SendDimensionsToClient> {
 
     @Override
     public void encode(FriendlyByteBuf buffer) {
+        buffer.writeNbt(registry.writeToNBT(new CompoundTag(), registry.getBonfires()));
         buffer.writeVarInt(dimensions.size());
         for (int i = 0; i < dimensions.size(); ++i) {
             buffer.writeResourceLocation(dimensions.get(i).location());
@@ -45,6 +53,6 @@ public class SendDimensionsToClient extends Packet<SendDimensionsToClient> {
 
     @Override
     public void handle(NetworkEvent.Context context) {
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientPacketHandler.setDimensionsFromServer(this));
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientPacketHandler.setBonfiresFromServer(this));
     }
 }

Wehavecookies56/Bonfires

src/main/java/wehavecookies56/bonfires/blocks/AshBonePileBlock.java
@@ -31,7 +31,8 @@ import net.minecraft.world.IWorldReader;
 import net.minecraft.world.World;
 import net.minecraftforge.common.util.Constants;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
-import wehavecookies56.bonfires.*;
+import wehavecookies56.bonfires.BonfiresConfig;
+import wehavecookies56.bonfires.bonfire.Bonfire;
 import wehavecookies56.bonfires.bonfire.BonfireRegistry;
 import wehavecookies56.bonfires.data.BonfireHandler;
 import wehavecookies56.bonfires.data.EstusHandler;
@@ -129,6 +130,11 @@ public class AshBonePileBlock extends Block {
                             PacketHandler.sendTo(new SyncEstusData(EstusHandler.getHandler(player)), (ServerPlayerEntity) player);
                             world.sendBlockUpdated(pos, state, state, Constants.BlockFlags.BLOCK_UPDATE);
                             return ActionResultType.SUCCESS;
+                        } else {
+                            //Bonfire lit but not in data, so should not be lit
+                            te.setLit(false);
+                            world.sendBlockUpdated(pos, state, state, Constants.BlockFlags.BLOCK_UPDATE);
+                            return ActionResultType.SUCCESS;
                         }
                     } else {
                         return ActionResultType.SUCCESS;
@@ -189,10 +195,13 @@ public class AshBonePileBlock extends Block {
                         world.addFreshEntity(fragment);
                     }
                     if (te.isLit()) {
-                        te.destroyBonfire(te.getID());
                         MinecraftServer server = ServerLifecycleHooks.getCurrentServer();
+                        Bonfire destroyed = BonfireHandler.getServerHandler(server).getRegistry().getBonfire(te.getID());
+                        te.destroyBonfire(te.getID());
                         BonfireHandler.getServerHandler(server).removeBonfire(te.getID());
                         PacketHandler.sendToAll(new SyncSaveData(BonfireHandler.getServerHandler(server).getRegistry().getBonfires()));
+                        PacketHandler.sendToAll(new SendBonfiresToClient());
+                        PacketHandler.sendToAll(new DeleteScreenshot(te.getID(), destroyed.getName()));
                     }
                 }
             }

src/main/java/wehavecookies56/bonfires/bonfire/Bonfire.java
@@ -4,6 +4,7 @@ import net.minecraft.util.RegistryKey;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
+import java.time.Instant;
 import java.util.UUID;
 
 /**
@@ -18,13 +19,16 @@ public class Bonfire {
     RegistryKey<World> dimension;
     boolean isPublic;
 
-    public Bonfire(String name, UUID id, UUID owner, BlockPos pos, RegistryKey<World> dimension, boolean isPublic) {
+    Instant timeCreated;
+
+    public Bonfire(String name, UUID id, UUID owner, BlockPos pos, RegistryKey<World> dimension, boolean isPublic, Instant timeCreated) {
         this.name = name;
         this.id = id;
         this.owner = owner;
         this.pos = pos;
         this.dimension = dimension;
         this.isPublic = isPublic;
+        this.timeCreated = timeCreated;
     }
 
     public String getName() {
@@ -73,5 +77,13 @@ public class Bonfire {
 
     public void setPublic(boolean aPublic) {
         isPublic = aPublic;
+    }
+
+    public Instant getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(Instant timeCreated) {
+        this.timeCreated = timeCreated;
     }
 }

src/main/java/wehavecookies56/bonfires/packets/client/SendBonfiresToClient.java
@@ -1,5 +1,6 @@
 package wehavecookies56.bonfires.packets.client;
 
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.util.registry.Registry;
@@ -8,26 +9,32 @@ import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.network.NetworkEvent;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
+import wehavecookies56.bonfires.bonfire.BonfireRegistry;
 import wehavecookies56.bonfires.client.ClientPacketHandler;
+import wehavecookies56.bonfires.data.BonfireHandler;
 import wehavecookies56.bonfires.packets.Packet;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class SendDimensionsToClient extends Packet<SendDimensionsToClient> {
+public class SendBonfiresToClient extends Packet<SendBonfiresToClient> {
 
-    public SendDimensionsToClient(PacketBuffer buffer) {
+    public SendBonfiresToClient(PacketBuffer buffer) {
         super(buffer);
     }
 
     public List<RegistryKey<World>> dimensions;
+    public BonfireRegistry registry;
 
-    public SendDimensionsToClient() {
+    public SendBonfiresToClient() {
         dimensions = new ArrayList<>(ServerLifecycleHooks.getCurrentServer().levelKeys());
+        registry = BonfireHandler.getServerHandler(ServerLifecycleHooks.getCurrentServer()).getRegistry();
     }
 
     @Override
     public void decode(PacketBuffer buffer) {
+        registry = new BonfireRegistry();
+        registry.readFromNBT(buffer.readNbt(), registry.getBonfires());
         dimensions = new ArrayList<>();
         int size = buffer.readVarInt();
         for (int i = 0; i < size; i++) {
@@ -37,6 +44,7 @@ public class SendDimensionsToClient extends Packet<SendDimensionsToClient> {
 
     @Override
     public void encode(PacketBuffer buffer) {
+        buffer.writeNbt(registry.writeToNBT(new CompoundNBT(), registry.getBonfires()));
         buffer.writeVarInt(dimensions.size());
         for (int i = 0; i < dimensions.size(); ++i) {
             buffer.writeResourceLocation(dimensions.get(i).location());
@@ -45,6 +53,6 @@ public class SendDimensionsToClient extends Packet<SendDimensionsToClient> {
 
     @Override
     public void handle(NetworkEvent.Context context) {
-        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientPacketHandler.setDimensionsFromServer(this));
+        DistExecutor.safeRunWhenOn(Dist.CLIENT, () -> ClientPacketHandler.setBonfiresFromServer(this));
     }
 }