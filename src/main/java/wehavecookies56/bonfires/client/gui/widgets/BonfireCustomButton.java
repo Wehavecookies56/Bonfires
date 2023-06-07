999f46b25ab19a5c72866c934eb6931f365814fa
Wehavecookies56 <qwenit56@gmail.com>
2023-06-07 14:11:09 +0100

    Extra fixes for 1.19.2

--- src/main/java/wehavecookies56/bonfires/client/gui/widgets/BonfireCustomButton.java
+++ src/main/java/wehavecookies56/bonfires/client/gui/widgets/BonfireCustomButton.java
@@ -31,7 +31,7 @@ public class BonfireCustomButton extends Button {
 
     ButtonType type;
     public BonfireCustomButton(int buttonId, int x, int y, ButtonType type, OnPress onPress) {
-        super(new Builder(Component.empty(), onPress).pos(x, y).size(16, 16));
+        super(x, y, 16, 16, Component.empty(), onPress);
         this.id = buttonId;
         this.type = type;
     }
@@ -40,14 +40,14 @@ public class BonfireCustomButton extends Button {
     public void render(PoseStack stack, int mouseX, int mouseY, float partialTicks) {
         if (visible) {
             RenderSystem.setShaderColor(1, 1, 1, 1);
-            if (mouseX >= getX() && mouseX <= getX() + width && mouseY >= getY() && mouseY <= getY() + height) {
+            if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
                 RenderSystem.setShaderColor(1, 1, 1, 1);
             } else {
                 RenderSystem.setShaderColor(0.8F, 0.8F, 0.8F, 1F);
             }
             RenderSystem.setShaderTexture(0, TRAVEL_TEX);
-            blit(stack, getX(), getY(), type.u, type.v, width, height);
-            if (mouseX >= getX() && mouseX <= getX() + width && mouseY >= getY() && mouseY <= getY() + height) {
+            blit(stack, x, y, type.u, type.v, width, height);
+            if (mouseX >= x && mouseX <= x + width && mouseY >= y && mouseY <= y + height) {
                 List<FormattedCharSequence> lines = new ArrayList<>();
                 lines.add(Component.translatable(type.translationKey).getVisualOrderText());
                 Minecraft.getInstance().screen.renderTooltip(stack, lines, mouseX, mouseY);
