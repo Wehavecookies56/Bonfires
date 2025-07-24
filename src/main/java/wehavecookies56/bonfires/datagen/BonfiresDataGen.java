c1b6b4cba6a53b7d277989fa47eb5856cf2ff40c
Wehavecookies56 <qwenit56@gmail.com>
2025-07-24 17:48:57 +0100

    datagen finally ported for fabric

--- src/main/java/wehavecookies56/bonfires/datagen/BonfiresDataGen.java
+++ src/main/java/wehavecookies56/bonfires/datagen/BonfiresDataGen.java
@@ -1,68 +1,49 @@
 package wehavecookies56.bonfires.datagen;
-//TODO port datagen to fabric
-/*
-import net.minecraft.data.DataGenerator;
-import net.minecraft.data.loot.BlockLootSubProvider;
-import net.minecraft.data.loot.LootTableProvider;
-import net.minecraft.world.flag.FeatureFlags;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.storage.loot.LootPool;
-import net.minecraft.world.level.storage.loot.LootTable;
-import net.minecraft.world.level.storage.loot.entries.LootItem;
-import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction;
-import net.minecraft.world.level.storage.loot.parameters.LootContextParamSets;
-import net.minecraft.world.level.storage.loot.providers.number.ConstantValue;
-import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator;
-import net.minecraftforge.common.data.ExistingFileHelper;
-import net.minecraftforge.data.event.GatherDataEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.RegistryObject;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricBlockLootTableProvider;
+import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
+import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.function.SetCountLootFunction;
+import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
+import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import wehavecookies56.bonfires.setup.BlockSetup;
 import wehavecookies56.bonfires.setup.ItemSetup;
 
-import java.util.Collections;
-import java.util.List;
-import java.util.stream.Collectors;
+public class BonfiresDataGen implements DataGeneratorEntrypoint {
 
-@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-public class BonfiresDataGen {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+        FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
 
-    @SubscribeEvent
-    public static void gatherData(GatherDataEvent event) {
-        DataGenerator generator = event.getGenerator();
-        final ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
-
-        generator.addProvider(true, new Recipes(generator));
-        generator.addProvider(true, new LootTableProvider(generator.getPackOutput(), Collections.emptySet(), List.of(new LootTableProvider.SubProviderEntry(BonfiresBlockLoot::new, LootContextParamSets.BLOCK))));
+        pack.addProvider(BonfiresBlockLoot::new);
+        pack.addProvider(Recipes::new);
     }
 
-    public static class BonfiresBlockLoot extends BlockLootSubProvider {
+    public static class BonfiresBlockLoot extends FabricBlockLootTableProvider {
 
-        protected BonfiresBlockLoot() {
-            super(Collections.emptySet(), FeatureFlags.REGISTRY.allFlags());
+
+        protected BonfiresBlockLoot(FabricDataOutput dataOutput) {
+            super(dataOutput);
         }
 
         @Override
-        protected void generate() {
-            dropWhenSilkTouch(BlockSetup.ash_block.get());
-            add(BlockSetup.ash_block.get(), new LootTable.Builder().withPool(
+        public void generate() {
+            dropsWithSilkTouch(BlockSetup.ash_block);
+            addDrop(BlockSetup.ash_block, new LootTable.Builder().pool(
                     new LootPool.Builder()
-                            .setRolls(ConstantValue.exactly(1))
-                            .add(LootItem.lootTableItem(ItemSetup.ash_pile.get())
-                                    .apply(SetItemCountFunction.setCount(UniformGenerator.between(3, 6))))
+                            .rolls(ConstantLootNumberProvider.create(1))
+                            .with(ItemEntry.builder(ItemSetup.ash_pile)
+                                    .apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(3, 6))))
             ));
-            add(BlockSetup.ash_bone_pile.get(), new LootTable.Builder().withPool(
+            addDrop(BlockSetup.ash_bone_pile, new LootTable.Builder().pool(
                     new LootPool.Builder()
-                            .setRolls(ConstantValue.exactly(1))
-                            .add(LootItem.lootTableItem(BlockSetup.ash_bone_pile.get()))
+                            .rolls(ConstantLootNumberProvider.create(1))
+                            .with(ItemEntry.builder(BlockSetup.ash_bone_pile))
             ));
-        }
-
-        @Override
-        protected Iterable<Block> getKnownBlocks() {
-            return BlockSetup.BLOCKS.getEntries().stream().map(RegistryObject::get).collect(Collectors.toList());
         }
     }
 }
-*/
