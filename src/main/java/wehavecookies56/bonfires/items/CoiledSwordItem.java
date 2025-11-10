1acd516303c8929cff6cc2eab6bbbeb6bf6c1e7d
Estelle <qwenit56@gmail.com>
2025-11-10 20:14:05 +0000

    set coiled sword repair ingredient fixes #63

--- src/main/java/wehavecookies56/bonfires/items/CoiledSwordItem.java
+++ src/main/java/wehavecookies56/bonfires/items/CoiledSwordItem.java
@@ -13,6 +13,7 @@ import net.minecraft.screen.slot.Slot;
 import net.minecraft.sound.SoundCategory;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ClickType;
+import wehavecookies56.bonfires.setup.ItemSetup;
 
 public class CoiledSwordItem extends SwordItem {
 
@@ -39,7 +40,7 @@ public class CoiledSwordItem extends SwordItem {
 
         @Override
         public Ingredient getRepairIngredient() {
-            return null;
+            return Ingredient.ofItems(ItemSetup.titanite_shard);
         }
 
         @Override
